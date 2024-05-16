f63b4d653f6288bdd48920270c6dff66966345f9
muhammadali010 <muhammadjonovmuhammadali32@gmail.com>
2024-05-16 16:11:52 +0500

    finish js

---
+++ js/script.js
@@ -0,0 +1,135 @@
+// 1chi masala
+// let L =+ prompt("uzunlikni kiriitng");
+// let m = L / 100;
+// m = parseInt(m)
+// console.log(m);
+// document.write(m);
+
+
+// 2chi masala
+// let m =+ prompt("kgni kiriting"); 
+// let t = m / 1000;
+// t = parseInt(t);
+// console.log(t);
+// document.write(t);
+
+
+// 3chi masala
+// let b =+ prompt("baytni kiriting");
+// let k = b/1024;
+// k = parseInt(k);
+// console.log(k);
+// document.write(k);
+
+
+// 4chi masala
+// let a =+ prompt("a ni kiriting");
+// let b =+ prompt("b ni kiriting");
+// let c = a / b ;
+// c = parseInt(c);
+// console.log(c);
+// document.write(c);
+
+
+// 6chi masala
+// let number =+prompt("ikki xonali son kiriting");
+// let a = number/10;
+// let b = number%10;
+// a =parseInt(a);
+// b =parseInt(b);
+// document.write(a);
+// document.write("va");
+// document.write(b);
+
+
+// 7chi masala
+// let number =+prompt("ikki xonali son kiriting");
+// let a = number%10;
+// let b = number/10;
+// a =parseInt(a);
+// b =parseInt(b);
+// document.write(a+b);
+
+
+// 8chi masala
+// let son =+prompt("ikki xonali son kiriting");
+// let a = son/10;
+// let b = son%10;
+// a= parseInt(a);
+// b = parseInt(b);
+// document.write(b);
+// document.write(a);
+// document.write("va")
+// document.write(a+b);
+
+
+// 9chi masala
+// let son =+ prompt("uch xonali son kiriting");
+// let a = son / 100;
+// a = parseInt(a);
+// document.write(a);
+
+
+// 10chi masala
+// let son =+ prompt("uch xonali son kiriting");
+// let a = son /10;
+// let b = a / 10;
+// let c = a % 10;
+// let d = son %100;
+// let e = d % 10;
+// a = parseInt(a);
+// b = parseInt(b);
+// c = parseInt(c);
+// d = parseInt(d);
+// e = parseInt(e);
+// document.write(e);
+// document.write(c);
+
+
+// 11chi masala
+// let son =+ prompt("uch xonali son kiriting");
+// let a = son /10;
+// let b = a / 10;
+// let c = a % 10;
+// let d = son %100;
+// let e = d % 10;
+// a = parseInt(a);
+// b = parseInt(b);
+// c = parseInt(c);
+// d = parseInt(d);
+// e = parseInt(e);
+// document.write(b+c+e);
+
+
+// 12chi masala
+// let son =+ prompt("uch xonali son kiriting");
+// let a = son /10;
+// let b = a / 10;
+// let c = a % 10;
+// let d = son %100;
+// let e = d % 10;
+// a = parseInt(a);
+// b = parseInt(b);
+// c = parseInt(c);
+// d = parseInt(d);
+// e = parseInt(e);
+// document.write(e);
+// document.write(c);
+// document.write(b);
+
+
+// 13chi masala
+// let son =+ prompt("uch xonali son kiriting");
+// let a = son /10;
+// let b = a / 10;
+// let c = a % 10;
+// let d = son %100;
+// let e = d % 10;
+// a = parseInt(a);
+// b = parseInt(b);
+// c = parseInt(c);
+// d = parseInt(d);
+// e = parseInt(e);
+// document.write(c);
+// document.write(e);
+// document.write(b);
